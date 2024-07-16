52916907fb4ea26f1f5d80c2ce716785921b475a
elaine-0412 <70514665+elaine-0412@users.noreply.github.com>
2024-07-16 09:49:58 +0800

    upd

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base:'VUE-猜拳遊戲',
+  base:'rock-game',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
